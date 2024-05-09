151494c74eee5c42fcf9390dbe7b4cef38bb6354
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2024-05-09 14:16:12 +0300

    blocking unusefull routers

--- routes/api/shop.js
+++ routes/api/shop.js
@@ -37,39 +37,39 @@ const {
 const router = express.Router();
 
 router.get("/newbd", ctrlWrapper(getShop));
-router.get("/ua/newbd", ctrlWrapper(getShop_ua));
-router.get("/ru/newbd", ctrlWrapper(getShop_ru));
-router.get("/en/newbd", ctrlWrapper(getShop_en));
-router.get("/de/newbd", ctrlWrapper(getShop_de));
+// router.get("/ua/newbd", ctrlWrapper(getShop_ua));
+// router.get("/ru/newbd", ctrlWrapper(getShop_ru));
+// router.get("/en/newbd", ctrlWrapper(getShop_en));
+// router.get("/de/newbd", ctrlWrapper(getShop_de));
 
 router.get("/", ctrlWrapper(getShopByFilter));
-router.get("/ua", ctrlWrapper(getShopByFilter_ua));
-router.get("/ru", ctrlWrapper(getShopByFilter_ru));
-router.get("/de", ctrlWrapper(getShopByFilter_de));
-router.get("/en", ctrlWrapper(getShopByFilter_en));
+// router.get("/ua", ctrlWrapper(getShopByFilter_ua));
+// router.get("/ru", ctrlWrapper(getShopByFilter_ru));
+// router.get("/de", ctrlWrapper(getShopByFilter_de));
+// router.get("/en", ctrlWrapper(getShopByFilter_en));
 
 router.get("/discount", ctrlWrapper(getDiscountShop));
-router.get("/ua/discount", ctrlWrapper(getDiscountShop_ua));
-router.get("/ru/discount", ctrlWrapper(getDiscountShop_ru));
-router.get("/de/discount", ctrlWrapper(getDiscountShop_de));
-router.get("/en/discount", ctrlWrapper(getDiscountShop_en));
+// router.get("/ua/discount", ctrlWrapper(getDiscountShop_ua));
+// router.get("/ru/discount", ctrlWrapper(getDiscountShop_ru));
+// router.get("/de/discount", ctrlWrapper(getDiscountShop_de));
+// router.get("/en/discount", ctrlWrapper(getDiscountShop_en));
 
 router.get("/rate", ctrlWrapper(getRateShop));
-router.get("/ua/rate", ctrlWrapper(getRateShop_ua));
-router.get("/ru/rate", ctrlWrapper(getRateShop_ru));
-router.get("/de/rate", ctrlWrapper(getRateShop_de));
-router.get("/en/rate", ctrlWrapper(getRateShop_en));
+// router.get("/ua/rate", ctrlWrapper(getRateShop_ua));
+// router.get("/ru/rate", ctrlWrapper(getRateShop_ru));
+// router.get("/de/rate", ctrlWrapper(getRateShop_de));
+// router.get("/en/rate", ctrlWrapper(getRateShop_en));
 
 router.get("/byid/:id/", ctrlWrapper(getShopById));
-router.get("/ua/byid/:id/", ctrlWrapper(getShopById_ua));
-router.get("/ru/byid/:id/", ctrlWrapper(getShopById_ru));
-router.get("/de/byid/:id/", ctrlWrapper(getShopById_de));
-router.get("/en/byid/:id/", ctrlWrapper(getShopById_en));
+// router.get("/ua/byid/:id/", ctrlWrapper(getShopById_ua));
+// router.get("/ru/byid/:id/", ctrlWrapper(getShopById_ru));
+// router.get("/de/byid/:id/", ctrlWrapper(getShopById_de));
+// router.get("/en/byid/:id/", ctrlWrapper(getShopById_en));
 
 router.get("/discounts", ctrlWrapper(getShopByFilterDiscounts));
-router.get("/ua/discounts", ctrlWrapper(getShopByFilterDiscounts_ua));
-router.get("/ru/discounts", ctrlWrapper(getShopByFilterDiscounts_ru));
-router.get("/de/discounts", ctrlWrapper(getShopByFilterDiscounts_de));
-router.get("/en/discounts", ctrlWrapper(getShopByFilterDiscounts_en));
+// router.get("/ua/discounts", ctrlWrapper(getShopByFilterDiscounts_ua));
+// router.get("/ru/discounts", ctrlWrapper(getShopByFilterDiscounts_ru));
+// router.get("/de/discounts", ctrlWrapper(getShopByFilterDiscounts_de));
+// router.get("/en/discounts", ctrlWrapper(getShopByFilterDiscounts_en));
 
 module.exports = routerShop = router;
